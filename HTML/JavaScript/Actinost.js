29577bf6a7838c72489cf16ffdf822a9999b0f93
Пешков Александр Алексеевич <apeshkov@cbg.local>
2018-04-25 10:36:06 +0300

    first Script

--- HTML/JavaScript/Actinost.js
+++ HTML/JavaScript/Actinost.js
@@ -1,12 +1,12 @@
 var calculate = function() {
-var period = document.getElementById('#period').value;
-var activ =  document.getElementById('#activnost').value;
-var knownData =  document.getElementById('#knownData').value;
-var dataResult =  document.getElementById('#dataResult').value;
+var period = document.getElementById('period').value;
+var activ =  document.getElementById('activnost').value;
+var knownData =  document.getElementById('knownData').value;
+var dataResult =  document.getElementById('dataResult').value;
 var result = 0;
 
 result = period*activ*knownData*dataResult;
-
-document.getElementById('#result').value = result;
+alert(result);
+document.getElementById('result').value = result;
 
 }
