c833e6a1bc2da89f96a3202138aabf0467468668
Пешков Александр Алексеевич <apeshkov@cbg.local>
2018-04-26 17:14:52 +0300

    1

---
+++ HTML/JavaScript/EstimateActivnost.js
@@ -0,0 +1,13 @@
+function EstimateActiv() {
+var D1 = document.getElementById('PowerDose').value;
+var x1 = document.getElementById('DistanceToSource').value;
+var CF7 =  document.getElementById('CF7').value;
+var d = document.getElementById('DepthScreen');
+var d1_2 = 0;
+if (document.getElementsById('poglotit').checked){
+var d1_2 = document.getElementById('KoefficHalf').value;
+}
+alert('Run');
+var A = (D1*x1*x1)/(CF7**Math.pow(0.5, d/d1_2);)
+document.getElementById('ResultActivnost').value = A;
+}
--- HTML/JavaScript/mainPage.js
+++ HTML/JavaScript/mainPage.js
@@ -2,6 +2,6 @@ function showname () {
   var name = document.getElementById('file');
   alert('Selected file: ' + name.files.item(0).name);
 var a = document.getElementById('img');
-a.src = a.src.replace()'Picture/'+name.files.item(0).name);
-alert('1');
+a.src = 'Picture/' + name.files.item(0).name;
+alert('Picture/' + name.files.item(0).name);
 };
