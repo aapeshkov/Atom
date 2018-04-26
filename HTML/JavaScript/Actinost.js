018fee84c7a1918aa477eb563282e2682a42eb15
Пешков Александр Алексеевич <apeshkov@cbg.local>
2018-04-26 10:44:20 +0300

    Activnost.js 1

--- HTML/JavaScript/Actinost.js
+++ HTML/JavaScript/Actinost.js
@@ -7,9 +7,11 @@ var activ =  document.getElementById('activnost').value;
 var knownData =  document.getElementById('knownData').value;
 var dataResult =  document.getElementById('dataResult').value;
 var result = 0;
+var interval = dataResult - knownData;
+var stepen = (-interval/period);
 
-result = period*activ*knownData*dataResult;
-alert(result);
-document.getElementById('result').value = result;
+result = activ*Math.pow(2, stepen);
+alert(result.toFixed(3));
+document.getElementById('result').value = result.toFixed(3);
 
 }
