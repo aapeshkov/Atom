5102df3d1f9f7d533fea7e3bb38ae8845bd769d7
Пешков Александр Алексеевич <apeshkov@cbg.local>
2018-04-26 11:54:22 +0300

    12

--- HTML/JavaScript/Distance.js
+++ HTML/JavaScript/Distance.js
@@ -1,10 +1,10 @@
 var distanceToSource = function() {
-//var d1 = document.getElementById('PowerM1');
-//var d2 = document.getElementById('PowerM2');
-//var x = document.getElementById('distanceBetween');
-//var a = d1/d2;
-//var x1 = x/(1-a)
+var d1 = document.getElementById('PowerM1').value;
+var d2 = document.getElementById('PowerM2').value;
+var x = document.getElementById('distanceBetween').value;
+var a = d1/d2;
+var x1 = x/(1-a)
 
-//document.getElementById('M1toSource').value = x1.toFixed(2);
-alert('Run');
+document.getElementById('M1toSource').value = x1.toFixed(2);
+alert(d1);
 }
