4d64df2c86f96f1da1e63869d42899f76b4b9444
Гагаринский Андрей Алексеевич <agagarinskiy@cbg.local>
2018-04-26 18:31:36 +0300

    123

--- HTML/JavaScript/mainPage.js
+++ HTML/JavaScript/mainPage.js
@@ -1,19 +1,7 @@
 function showname () {
-<<<<<<< HEAD
-  var name = document.getElementById('fileInput');
-  alert('Selected file: ' + name.files.item(0).name);
-  alert('Selected file: ' + name.files.item(0).size);
-  alert('Selected file: ' + name.files.item(0).type);
-};
-
-var ChangeImage = function() {
-alert('Run');
-  }
-=======
   var name = document.getElementById('file');
   alert('Selected file: ' + name.files.item(0).name);
 var a = document.getElementById('img');
 a.src = 'Picture/' + name.files.item(0).name;
 alert('Picture/' + name.files.item(0).name);
 };
->>>>>>> 60ab003ebca60c05d79967af471d4f030d8b5f2f
