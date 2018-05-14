afb4612ac02850d0578cca53d6dbcb1eb27f231c
Пешков Александр Алексеевич <apeshkov@cbg.local>
2018-05-14 15:30:02 +0300

    Learn

---
+++ Nodejs/Learn/server.js
@@ -0,0 +1,18 @@
+var db = require('db');
+db.connect();
+var log = require('./logger')(module);
+
+var user = require('./user');
+function run() {
+var vasya = new user.User("Вася");
+var petya = new user.User("Петя");
+
+vasya.hello(petya);
+log(db.getPhrase("Run successful"));
+};
+
+if (module.parent) {
+exports.run = run;
+} else {
+  run();
+}
