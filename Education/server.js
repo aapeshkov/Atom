95641d9894880b155d102e9c40f34bbfe25261a7
Пешков Александр Алексеевич <apeshkov@cbg.local>
2018-05-21 12:38:49 +0300

    3

--- Education/server.js
+++ Education/server.js
@@ -45,6 +45,6 @@ var server = http.createServer(function(request, response){
   var absPath = './' + filePath;
   serverStatic(response, cache, absPath);
 })
-server.listen(300, function(){
+server.listen(3000, function(){
   console.log("Server listening on port 3000.");
 });
